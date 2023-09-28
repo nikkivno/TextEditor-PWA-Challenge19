400bef2d35bd0cb21c4876a033347bbfaec653f3
nikkivno <nikkivigneault@gmail.com>
2023-09-28 17:04:03 -0300

    added in info in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,49 +1,47 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-console.error('putDb not implemented');
   try {
-    const db = await initdb();
-    const tx = db.transation ('jate', 'readwrite');
-    const store = tx.objectStore('jate');
+    const db = await openDB("jate", 1);
+    const tx = db.transaction("jate", "readwrite");
+    const store = tx.objectStore("jate");
 
-    const result = await store.add(content);
+    const result = await store.put({ value: content, id: 1 });
 
-    console.log('Data added to the database', result);           
+    console.log("Data added to the database", result);
   } catch (error) {
-    console.error('Error adding data to the database', error);
+    console.error("Error adding data to the database", error);
   }
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error('getDb not implemented');
-try {
-  const db = await initdb();
-  const tx = db.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
+  try {
+    const db = await openDB("jate", 1);
+    const tx = db.transaction("jate", "readonly");
+    const store = tx.objectStore("jate");
 
-  const allData = await store.getAll();
+    const result = await store.get(1);
 
-  console.log('All data from the database:', allData);
-  return allData;
-} catch (error) {
-  console.error('Error getting data from the database:', error);
-  return [];
-}
+    console.log("All data from the database:", result);
+    return result?.value;
+  } catch (error) {
+    console.error("Error getting data from the database:", error);
+    return;
+  }
 };
 
 initdb();
